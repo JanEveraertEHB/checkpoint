398e9a8741c249a8a8f28182955e56474134c9d4
Jan Everaert <jan@crashlab.be>
2025-11-17 16:35:28 +0100

    deploy update

--- images/api/src/db/knexfile.js
+++ images/api/src/db/knexfile.js
@@ -16,5 +16,19 @@ module.exports = {
     }
   },
 
+  production: {
+    client: 'pg',
+    connection: {
+      host : process.env.POSTGRES_HOST,
+      port : 5432,
+      user : process.env.POSTGRES_USER,
+      password : process.env.POSTGRES_PASSWORD,
+      database : process.env.POSTGRES_DATABASE
+    },
+    pool: {
+      min: 2,
+      max: 10
+    }
+  }
 
 };
