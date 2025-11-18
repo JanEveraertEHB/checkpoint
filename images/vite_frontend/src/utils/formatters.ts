fb892194ed46e699747c995e1810feed207208f6
Jan Everaert <jan@crashlab.be>
2025-11-18 12:28:21 +0100

    feat: image upload view

--- images/vite_frontend/src/utils/formatters.ts
+++ images/vite_frontend/src/utils/formatters.ts
@@ -9,17 +9,29 @@ export const formatDate = (dateString: string): string => {
 }
 
 export const getImageUrl = (filename: string): string => {
-  const apiUrl = import.meta.env.VITE_API_URL || '/api'
-  // Remove /api suffix if present to get base URL
-  // const baseUrl = apiUrl.replace(/\/api\/?$/, '')
-  return `${apiUrl}/uploads/${filename}`
+  
+  if (window.location.href.includes("localhost")) {
+    const apiUrl = import.meta.env.VITE_API_URL || '/api'
+    // Remove /api suffix if present to get base URL
+    const baseUrl = apiUrl.replace(/\/api\/?$/, '')
+    console.log(baseUrl)
+    return `${baseUrl}/uploads/${filename}`
+  } else {
+    console.log(import.meta.env.VITE_API_URL )
+    if(import.meta.env.VITE_API_URL ) {
+      return `${import.meta.env.VITE_API_URL}/uploads/${filename}`
+    } else {
+      return ''
+    }
+  }
 }
 
 export const getDocumentUrl = (filename: string): string => {
   const apiUrl = import.meta.env.VITE_API_URL || '/api'
   // Remove /api suffix if present to get base URL
-  // const baseUrl = apiUrl.replace(/\/api\/?$/, '')
-  return `${apiUrl}/uploads/${filename}`
+  const baseUrl = apiUrl.replace(/\/api\/?$/, '')
+  console.log(baseUrl)
+  return `${baseUrl}/uploads/${filename}`
 }
 
 export const stripHtmlTags = (html: string): string => {
